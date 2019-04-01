aa55c5f32b32f8a9be94f0e73db8e6c753e730a3
muiscript <myam0118@gmail.com>
2019-04-01 22:26:15 +0900

    implement GetUserItems

--- miniqiita.go
+++ miniqiita.go
@@ -2,10 +2,16 @@ package miniqiita
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"path"
+	"strconv"
 )
 
 type Client struct {
@@ -45,5 +51,52 @@ type Item struct {
 // GET /api/v2/users/:user_id/items
 // https://qiita.com/api/v2/docs#get-apiv2usersuser_iditems
 func (c *Client) GetUserItems(ctx context.Context, userID string, page, perPage int) ([]*Item, error) {
+	reqURL := *c.BaseURL
+
+	// set path
+	reqURL.Path = path.Join(reqURL.Path, "users", userID, "items")
+
+	// set query
+	q := reqURL.Query()
+	q.Add("page", strconv.Itoa(page))
+	q.Add("per_page", strconv.Itoa(perPage))
+	reqURL.RawQuery = q.Encode()
+
+	// instantiate request
+	req, err := http.NewRequest(http.MethodGet, reqURL.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// set header
+	req.Header.Set("User-Agent", "qiita-go-client")
+
+	// set context
+	req = req.WithContext(ctx)
+
+	// send request
+	resp, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	switch resp.StatusCode {
+	case http.StatusOK:
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+		var items []*Item
+		if err := json.Unmarshal(bodyBytes, &items); err != nil {
+			return nil, err
+		}
+		return items, nil
+	case http.StatusBadRequest:
+		return nil, errors.New("bad request. some parameters may be invalid")
+	case http.StatusNotFound:
+		return nil, fmt.Errorf("not found. user with id '%s' may not exist", userID)
+	}
+
 	return nil, nil
 }
